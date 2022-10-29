3c0bdbbb45f45b03e8e0de0264d5e9077a71cfaf
rhidalgoc <r_hidalgo_c@hotmail.com>
2022-10-29 20:04:50 -0300

    Segundo commit

--- src/app/component/listado-imagenes/listado-imagenes.component.ts
+++ src/app/component/listado-imagenes/listado-imagenes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-imagenes',
@@ -10,6 +10,35 @@ export class ListadoImagenesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  @Input()
+  carnes:boolean = false;
+
+
+  playSound(num: number){
+
+    let audio = new Audio();
+
+    if(num == 1) {
+      audio.src = "../../../assets/sonidos/vaca 3.wav";
+    }
+
+    if(num == 2) {
+      audio.src = "../../../assets/sonidos/chancho 5.wav";
+    }
+
+    if(num == 3) {
+      audio.src = "../../../assets/sonidos/gallina 2.wav";
+    }
+
+    if(num == 4) {
+      audio.src = "../../../assets/sonidos/PATO.mp3";
+    }
+
+    audio.load();
+    audio.play();
   }
 
 }
